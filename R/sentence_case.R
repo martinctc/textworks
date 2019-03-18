0187d4be44ac591c31e00645dc838b8ccea7c64e
Martin Chan <martinchan53@gmail.com>
2019-03-18 23:24:17 +0000

    Add sentence_case

---
+++ R/sentence_case.R
@@ -0,0 +1,17 @@
+#' Convert a string into sentence case
+#' 
+#' @description 
+#' (^|\\.) matches start of string OR literally a fullstop
+#' \\s* matches whitespace symbols
+#' (.) matches any character but a new line
+#' \\1 Back-references Group 1
+#' \\2 Turns Group 2 into uppercase
+#'
+#' @examples 
+#' sentence_case("i'm not hundred percent sure. why not. cool!")
+#' @export
+sentence_case <- function(string){
+  trimws(gsub("(^|\\.)\\s*(.)", "\\1 \\U\\2",
+              string, perl=TRUE))
+}
+
