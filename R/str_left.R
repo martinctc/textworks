5fa117a2292a30325acd1f7e76e5944e6da14db5
Martin Chan <martinchan53@gmail.com>
2020-02-08 16:47:25 +0000

    Added str_left()

---
+++ R/str_left.R
@@ -0,0 +1,14 @@
+#' Return the n number of characters from the left
+#'
+#' Wrapper convenience function
+#' Analagous to `str_right()`
+#'
+#' @param x String character to pass through
+#' @param n Integer specifying the number of characters to return from the left.
+#'
+#' @importFrom stringr str_sub
+#'
+#' @export
+str_left <- function(x, n){
+  str_sub(x, 1, n)
+}
