bcc71dcf18c78f37ae5ae2093ab1e04b36c211b2
Martin Chan <martinchan53@gmail.com>
2020-04-24 21:06:19 +0100

    Fix R CMD run notes and warnings

--- R/tokenise_ngram.R
+++ R/tokenise_ngram.R
@@ -3,6 +3,8 @@
 #' @param x Vector of character string
 #' @param n Specifying the n for n-grams, e.g. 2 for bi-grams
 #' @param collapse Specify what to use for separating the outcome
+#' @importFrom NLP words
+#' @importFrom NLP ngrams
 #'
 #' @export
 tokenise_ngram <- function(x, n = 2, collapse = " ") {
