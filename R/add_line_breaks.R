e2a604f1800f65d86c65d54047d5a34bb2bacdf2
Martin Chan <martinchan53@gmail.com>
2022-10-06 10:16:35 +0100

    feat: add new option for `add_line_breaks()`

--- R/add_line_breaks.R
+++ R/add_line_breaks.R
@@ -1,29 +1,80 @@
-#' Split long sentences into multiple lines by adding line breaks
+#' @title
+#' Split long strings into multiple lines by adding line breaks
+#'
+#' @description Wrap text in visualizations according to a preset character
+#'   threshold, or the maximum number of words allowed in the first line. The
+#'   next space in the string is replaced with `\n`, which will render as next
+#'   line in plots and messages.
 #'
 #' @param string Pass text string here.
-#' @param words The maximum number of words allowed in the first line
+#' @param nword numeric, defaults to NULL. The maximum number of words allowed in the first line. Only one
+#'   argument from either `nword` or `nchar` is used, otherwise an error is
+#'   generated.
+#' @param nchar numeric, defaults to NULL. Number of character units by which the
+#'   next space would be replaced with `\n` to move text to next line. Only one
+#'   argument from either `nword` or `nchar` is used, otherwise an error is
+#'   generated.
 #' @examples
-#' library(stringr)
-#' library(magrittr)
-#' add_line_breaks("This is a lovely cup of tea",2)
-#' add_line_breaks("This is a lovely cup of tea",3)
-#' add_line_breaks("This is a lovely cup of tea",1)
-#' add_line_breaks("Oh wow. Amazing. I cannot believe that this is actually possible!",4)
-#' paste0("word",1:100, collapse = " ") %>% add_line_breaks(3)
+#' add_line_breaks("This is a lovely cup of tea", nword = 2)
+#'
+#' add_line_breaks("This is a lovely cup of tea", nword = 3)
+#'
+#' # Display text in a plot
+#' plot.new()
+#' text(
+#'   x = 0.5,
+#'   y = 0.5,
+#'   labels = add_line_breaks("This is a lovely cup of tea", nword = 3),
+#'   cex = 1.6
+#' )
+#'
+#' # Applying to a long string
+#' paste0("word",1:100, collapse = " ") %>% add_line_breaks(nword = 3)
+#'
+#' # Define breaks using characters
+#' add_line_breaks("Oh wow. Amazing. I cannot believe that this is actually possible!", nchar = 10)
+#'
 #' @export
 #'
-add_line_breaks <-
-  function(string, words) {
+add_line_breaks <-  function(string, nword = NULL, nchar = NULL) {
+
+  if(is.null(nword) & is.null(nchar)){
+
+    stop("Please provide a valid value to either `nword` or `nchar`.")
+
+  } else if(!is.null(nword) & !is.null(nchar)){
+
+    stop(
+      "Please only supply a value to either `nword` or `nchar`, not both"
+      )
+
+  } else if(!is.null(nword)){
+
     words_list <- unlist(stringr::str_split(string, " "))
 
     # Return index of spaces to insert line break
-    ind <- which(sapply(1:nwords(string), function(x) x %%  words) == 0)
+    ind <- which(sapply(1:nwords(string), function(x) x %%  nword) == 0)
 
     for(i in 0:(length(ind)-1)){
-        new_index <- ind[i + 1] + i
+      new_index <- ind[i + 1] + i
 
       words_list <- append(words_list, "\n", after = new_index)
     }
 
-    return(paste0(words_list, collapse = " "))
+    paste0(words_list, collapse = " ")
+
+  } else if(!is.null(nchar)){
+
+    patt <- paste0(
+      '(.{1,',
+      nchar,
+      '})(\\s|$)'
+    )
+
+    gsub(
+      pattern = patt,
+      replacement = '\\1\n',
+      x = string
+    )
+  }
   }
